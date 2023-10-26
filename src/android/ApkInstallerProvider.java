eb73bc938e93a5b79535494fe8ff363d10fd492a
Fabrizio1985 <fabrizio.tiracorrendo@gmail.com>
2023-10-26 15:46:02 +0200

    New library

--- src/android/ApkInstallerProvider.java
+++ src/android/ApkInstallerProvider.java
@@ -1,6 +1,6 @@
 package android;
 
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 
 public class ApkInstallerProvider extends FileProvider {
    // avoid android:authrities conflict with other lib which also use android.support.v4.content.FileProvider
