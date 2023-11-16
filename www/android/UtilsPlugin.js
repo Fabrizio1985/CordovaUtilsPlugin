5b63db2e365725fca366225e31418ed2963b02cc
Fabrizio1985 <fabrizio.tiracorrendo@gmail.com>
2023-11-16 10:30:57 +0100

    Add index.d.ts

--- www/android/UtilsPlugin.js
+++ www/android/UtilsPlugin.js
@@ -1,111 +1,109 @@
 module.exports = {
 
 	exit: () => {
-		return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", "exit", [] ) );
+		return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", "exit", []));
 	},
 
 	android: {
 		resolveUri: (uri) => {
-			return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", "resolveUri", [uri] ) );
+			return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", "resolveUri", [uri]));
+		},
+		sdkVersion: () => {
+			return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", 'sdkVersion', []));
+		},
+		uploadGoogle: (path) => {
+			return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", 'uploadGoogle', [path]));
 		}
 	},
 
-    executeCommand: ( commands ) => {
-        return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", "executeCommand", [commands] ) );
-    },
-
-    createFile: ( name, data ) => {
-        const nameTemp = typeof name === 'string' ? name : name.android;
-        return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", "createFile", [nameTemp, data] ) );
-    },
-
-    readFile: ( name ) => {
-        const nameTemp = typeof name === 'string' ? name : name.android;
-        return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", "readFile", [nameTemp] ) );
-    },
-    
-    removeFile: (path) => {
-		 return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", 'removeFile', [path] ) );
+	executeCommand: (commands) => {
+		return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", "executeCommand", [commands]));
 	},
-	
+
+	createFile: (name, data) => {
+		const nameTemp = typeof name === 'string' ? name : name.android;
+		return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", "createFile", [nameTemp, data]));
+	},
+
+	readFile: (name) => {
+		const nameTemp = typeof name === 'string' ? name : name.android;
+		return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", "readFile", [nameTemp]));
+	},
+
+	removeFile: (path) => {
+		return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", 'removeFile', [path]));
+	},
+
 	writeFile: (path, data) => {
-		 return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", 'writeFile', [path, data] ) );
+		return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", 'writeFile', [path, data]));
 	},
 
-    localStorage: {
+	localStorage: {
 
-        getItem: ( key ) => {
-            return Promise.resolve( window.localStorage.getItem( key ) );
-        },
+		getItem: (key) => {
+			return Promise.resolve(window.localStorage.getItem(key));
+		},
 
-        setItem: ( key, data ) => {
-            Promise.resolve( window.localStorage.setItem( key, data ) );
-        },
+		setItem: (key, data) => {
+			Promise.resolve(window.localStorage.setItem(key, data));
+		},
 
-        removeItem: ( key ) => {
-            Promise.resolve( window.localStorage.removeItem( key ) );
-        }
-    },
-
-    installUpdate: ( blob ) => {
-        
-        return new Promise( ( resolve, reject ) => {
-            const reader = new FileReader();
-            reader.readAsDataURL( blob );
-            reader.onloadend = () => resolve( reader.result.replace( 'data:application/vnd.android.package-archive;base64,', '' ) );
-            reader.onerror = () => reject( reader.error );
-        } ).then( dataBase64 => {
-            return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", "installUpdate", [dataBase64] ) );
-        });
-    },
-
-    getTempPath: () => {
-        return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", 'getTempPath', [] ) );
-    },
-
-    getPlatform: () => {
-        return 'android';
-    },
-    
-    getAppVersion: () => {
-        return cordova.getAppVersion.getVersionNumber();
-    },
-    
-    getUserDataFolder: () => {
-		 return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", 'getUserDataFolder', [] ) );
+		removeItem: (key) => {
+			Promise.resolve(window.localStorage.removeItem(key));
+		}
 	},
-	
+
+	installUpdate: (blob) => {
+
+		return new Promise((resolve, reject) => {
+			const reader = new FileReader();
+			reader.readAsDataURL(blob);
+			reader.onloadend = () => resolve(reader.result.replace('data:application/vnd.android.package-archive;base64,', ''));
+			reader.onerror = () => reject(reader.error);
+		}).then(dataBase64 => {
+			return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", "installUpdate", [dataBase64]));
+		});
+	},
+
+	getTempPath: () => {
+		return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", 'getTempPath', []));
+	},
+
+	getPlatform: () => {
+		return 'android';
+	},
+
+	getAppVersion: () => {
+		return cordova.getAppVersion.getVersionNumber();
+	},
+
+	getUserDataFolder: () => {
+		return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", 'getUserDataFolder', []));
+	},
+
 	pathJoin: (...path) => {
 		return path.join('/');
 	},
-	
+
 	checkFileExist: (path) => {
-		return new Promise(( resolve, reject ) => cordova.exec( (value) => {
+		return new Promise((resolve, reject) => cordova.exec((value) => {
 			resolve(value === 'true')
-		}, reject, "UtilsPlugin", 'checkFileExist', [path] ) );
+		}, reject, "UtilsPlugin", 'checkFileExist', [path]));
 	},
-	
+
 	createFolder: (path) => {
-		 return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", 'createFolder', [path] ) );
+		return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", 'createFolder', [path]));
 	},
-	
+
 	readFolder: (path) => {
-		 return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", 'readFolder', [path] ) );
+		return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", 'readFolder', [path]));
 	},
-	
+
 	createWriteStream: (path) => {
 		return null;
 	},
-	
-	uploadGoogle: (path) => {
-		return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", 'uploadGoogle', [path] ) );
-	},
-	
+
 	keepAwake: () => {
-		return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", 'keepAwake', [] ) );
-	},
-	
-	getSdkVersion: () => {
-		return new Promise(( resolve, reject ) => cordova.exec( resolve, reject, "UtilsPlugin", 'sdkVersion', [] ) );
+		return new Promise((resolve, reject) => cordova.exec(resolve, reject, "UtilsPlugin", 'keepAwake', []));
 	}
 }
---
+++ www/index.d.ts
@@ -0,0 +1,31 @@
+export interface CordovaUtils {
+	exit: () => Promise<void>;
+	executeCommand: (commands: any) => any;
+	createFile: (path: string, data: string) => Promise<string>;
+	readFile: (path: string | {android?: string, electron?:string}) => Promise<ArrayBuffer>;
+	removeFile: (path: string) => Promise<void>;
+	writeFile: (path: string, data: string) => Promise<void>;
+	installUpdate: (blob: Blob) => Promise<void>;
+	getTempPath: () => Promise<string>;
+	getPlatform: () => string;
+	getAppVersion: () => number;
+	getUserDataFolder: () => Promise<string>;
+	pathJoin: (...path: string[]) => string;
+	checkFileExist: (path: string) => Promise<true>;
+	createFolder: (path: string) => Promise<string>;
+	readFolder: (path: string) => Promise<Array<{name: string}>>;
+	createWriteStream(path: string): any;
+	keepAwake: () => Promise<void>;
+	
+	android: {
+		resolveUri: (uri: string) => Promise<string>;
+		sdkVersion: () => Promise<void>;
+		uploadGoogle: (path: string) => Promise<void>;
+	},
+	
+	localStorage: {
+		getItem: (key: string) => Promise<string>;
+		setItem:  (key: string, data: string) => Promise<void>;
+		removeItem: (key: string) => Promise<void>;
+	}
+}
